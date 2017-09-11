0f65121c61f378d569651953525015ed25ecd6a1
Josh Williams <josh@joshwilliams.com>
2017-09-11 13:02:01 -0700

    print ID to fixed div on page

---
+++ index.js
@@ -0,0 +1,24 @@
+var body = document.body;
+var messageEl = document.createElement('div');
+var re = /https:\/\/www.google.com\/maps\/@[-.\d]+,[-.\d]+\S+data=\S+!1s(\S+)!2e/;
+var result = re.exec(window.location.href);
+var message = "PanoID: ";
+if (result) {
+	message = message + result[1];
+} else {
+	message = "PanoID not found."
+}
+
+messageEl.innerHTML = message;
+body.appendChild(messageEl);
+messageEl.style.zIndex = 2147483647;
+messageEl.style.position = "fixed";
+messageEl.style.top = "0";
+messageEl.style.right = "0";
+messageEl.style.fontFamily = "Arial, sans-serif";
+messageEl.style.fontWeight = "bold";
+messageEl.style.fontSize = "24px";
+messageEl.style.backgroundColor = "#f4ff60";
+messageEl.style.color = '#000000';
+messageEl.style.padding = "20px 20px 20px 20px";
+console.log(message);
